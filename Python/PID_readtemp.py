fd4b4d7bf686b7fd350170f4647acb48d4c987a7
Mateusz Urbańczyk <murbanczyk@spektrino.com>
2022-03-17 10:11:20 +0100

    Updated communication for PID: UR32742A

--- Python/PID_readtemp.py
+++ Python/PID_readtemp.py
@@ -7,19 +7,22 @@ instrument.serial.timeout = 0.2
 instrument.serial.baudrate = 19200         # Baud
 instrument.serial.bytesize = 8
 instrument.serial.stopbits = 1
+#temperature set point adress 1200
+
+
 effect = instrument.write_register(3000,1, 0)  #Reset the display
 temperature=instrument.read_register(1000, 1, signed=True)  # Registernumber, number of decimals
 # print(temperature)
 # HeatingOn = (instrument.read_register(1008, 1))  # Heating on of
-heatingPower = instrument.read_register(1009, 0)  # Heating power 0-100%
-setPoint = instrument.read_register(1001, 1, signed=True)  # setpoint temperature
+heatingPower = instrument.read_register(1212, 0)  # Heating power 0-100%
+setPoint = instrument.read_register(1200, 1, signed=True)  # setpoint temperature
 # print(heatingPower)
 # print(setPoint)
 # for k in range(0,10):
-instrument.write_string(3009, " C", number_of_registers=1)
-instrument.write_string(3010, " A", number_of_registers=1)
-instrument.write_string(3011, " T", number_of_registers=1)
-effect = instrument.write_register(3000, 1, 0)  #Reset the display
+instrument.write_string(701, " C", number_of_registers=1)
+instrument.write_string(702, " A", number_of_registers=1)
+instrument.write_string(703, " T", number_of_registers=1)
+# effect = instrument.write_register(3000, 1, 0)  #Reset the display
 path1 = path+"\\temprecord.txt"
 file = open(path1, "w")
 file.write(str(temperature))
